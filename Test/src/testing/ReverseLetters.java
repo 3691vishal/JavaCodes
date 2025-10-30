51624f230f8650939ab48bf26925816bb0a3aa6f
Vishal Sharma <you@example.com>
2025-10-30 22:31:00 +0000

    spell check

--- Test/src/testing/ReverseLetters.java
+++ Test/src/testing/ReverseLetters.java
@@ -4,7 +4,7 @@ public class ReverseLetters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s1 = "Revese this String";
+		String s1 = "Reverse this String";
 		StringBuilder s2 = new StringBuilder();
 
 		s2.append(s1);
